e3253e61fb58b4275fd87829cace5a28c4ce17d0
rubenandrebarreiro <rubenandrebarreiro@192.168.1.5>
2019-10-07 12:32:39 -0700

    Class for Abstract Syntax Tree's Nodes of Atomic Values added

---
+++ src/abstractsyntaxtree/node/atomic/ASTNegativeNum.java
@@ -0,0 +1,60 @@
+package abstractsyntaxtree.node.atomic;
+
+/**
+ * Interpreter for Imperative/Functional Language
+ * 
+ * Interpretation and Compilation of Programming Languages
+ * 
+ * Faculty of Science and Technology of New University of Lisbon
+ * (FCT NOVA | FCT/UNL)
+ * 
+ * Integrated Master of Computer Science and Engineering
+ * (BSc. + MSc. Bologna Degree)
+ * 
+ * Academic Year 2019/2020
+ * 
+ */
+
+import abstractsyntaxtree.node.ASTNode;
+
+/**
+ * Class for the a Node of an Abstract Syntax Tree, representing an atomic negative number.
+ * 
+ * @supervisor Prof. Luis Manuel Caires - lcaires@fct.unl.pt
+ * 
+ * @author Eduardo Bras Silva (no. 41798) - emf.silva@campus.fct.unl.pt
+ * @author Ruben Andre Barreiro (no. 42648) - r.barreiro@campus.fct.unl.pt
+ *
+ */
+public class ASTNegativeNum implements ASTNode {
+
+	// Global Variables:
+	/**
+	 * The A.S.T. Node, representing an atomic negative number
+	 */
+	private int numASTNodeValue;
+		
+	
+	// Constructors:
+	/**
+	 * Constructor #1:
+	 * - The Constructor of a Node of an Abstract Syntax Tree.
+	 * 
+	 * @param numASTNodeValue the value of the A.S.T. Node, representing an atomic negative number
+	 */
+	public numASTNodeValue(int numASTNodeValue) {
+		this.numASTNodeValue = numASTNodeValue;
+	}
+	
+	
+	// Methods:
+	/**
+	 * Evaluates the current Node of an Abstract Syntax Tree, returning its atomic negative number.
+	 */
+	@Override
+	public int eval() {
+		
+		// Returns A.S.T. Node, representing an atomic negative number
+		return (-1) * this.numASTNodeValue;
+	}
+}
