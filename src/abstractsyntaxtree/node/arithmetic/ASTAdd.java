3bb8e7d2a623ff8466ce14b0dae4dc56d466e631
rubenandrebarreiro <rubenandrebarreiro@192.168.1.5>
2019-10-07 12:32:18 -0700

    Class for Abstract Syntax Tree's Nodes of Arithmetic Operations added

---
+++ src/abstractsyntaxtree/node/arithmetic/ASTAdd.java
@@ -0,0 +1,68 @@
+package abstractsyntaxtree.node.arithmetic;
+
+/**
+ * Interpreter for Imperative/Functional Language
+ * 
+ * Interpretation and Compilation of Programming Languages
+ * 
+ * Faculty of Science and Technology of New University of Lisbon
+ * (FCT NOVA | FCT/UNL)
+ * 
+ * Integrated Master of Computer Science and Engineering
+ * (BSc. + MSc. Bologna Degree)
+ * 
+ * Academic Year 2019/2020
+ * 
+ */
+import abstractsyntaxtree.node.ASTNode;
+
+/**
+ * Class for the a Node of an Abstract Syntax Tree, performing the Addition of its descendants.
+ * 
+ * @supervisor Prof. Luis Manuel Caires - lcaires@fct.unl.pt
+ * 
+ * @author Eduardo Bras Silva (no. 41798) - emf.silva@campus.fct.unl.pt
+ * @author Ruben Andre Barreiro (no. 42648) - r.barreiro@campus.fct.unl.pt
+ *
+ */
+public class ASTAdd implements ASTNode {
+	
+	// Global Variables:
+	/**
+	 * The left A.S.T. Node descendant
+	 */
+	private ASTNode leftASTNodeDescedant;
+	
+	/**
+	 * The right A.S.T. Node descendant
+	 */
+	private ASTNode rightASTNodeDescedant;
+	
+	
+	// Constructors:
+	/**
+	 * Constructor #1:
+	 * - The Constructor of a Node of an Abstract Syntax Tree.
+	 * 
+	 * @param leftASTNodeDescedant the left side Descendant of the A.S.T. Node
+	 * @param rightASTNodeDescedant the left side Descendant of the A.S.T. Node
+	 */
+	public ASTAdd(ASTNode leftASTNodeDescedant, ASTNode rightASTNodeDescedant) {
+		this.leftASTNodeDescedant = leftASTNodeDescedant;
+		this.rightASTNodeDescedant = rightASTNodeDescedant;
+	}
+	
+	
+	// Methods:
+	/**
+	 * Evaluates the current Node of an Abstract SyntaxTree, performing its Addition.
+	 */
+	@Override
+	public int eval() {
+		int leftASTNodeDescendantValue = leftASTNodeDescedant.eval();
+		int rightASTNodeDescedantValue = rightASTNodeDescedant.eval();
+		
+		// Returns the Addition of the A.S.T. Nodes Descendants
+		return leftASTNodeDescendantValue + rightASTNodeDescedantValue;
+	}
+}
