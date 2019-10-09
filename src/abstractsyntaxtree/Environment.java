36b186203cf220c91a05f7efbd98b32ed4ca0fd2
Eduardo Silva <emf.silva@campus.fct.unl.pt>
2019-10-09 17:42:55 +0100

    Add comments

--- src/abstractsyntaxtree/Environment.java
+++ src/abstractsyntaxtree/Environment.java
@@ -17,16 +17,33 @@ public class Environment {
 	private Environment ancestor;
 	private Map<String, Integer> map;
 	
+	// Constructors:
+	/**
+	 * Constructor #1:
+	 * - The Constructor of and environment with no ancestor.
+	 */
 	public Environment() {
 		map = new HashMap<String, Integer>();
 		ancestor = null;
 	}
 	
+	/**
+	 * Constructor #2:
+	 * - The Constructor of and environment with an ancestor.
+	 * 
+	 * @param ancestor the previous environment on the stack.
+	 */
 	public Environment(Environment ancestor) {
 		map = new HashMap<String, Integer>();
 		this.ancestor = ancestor;
 	}
 	
+	// Methods:
+	/**
+	 * Finds a value with a given ID.
+	 * @param id the identifier to search for.
+	 * @return the value associated with id.
+	 */
 	public int find(String id) {
 		Integer value = map.get(id);
 //		if(value == null) {
@@ -36,6 +53,11 @@ public class Environment {
 			return value;
 	}
 	
+	/**
+	 * Associates a given value with a given identifier.
+	 * @param id the identifier of the new value.
+	 * @param value the value of the new identifier.
+	 */
 	public void assoc(String id, int value)
 	{
 		if(map.containsKey(id)) {
@@ -46,10 +68,19 @@ public class Environment {
 		}
 	}
 	
+	/**
+	 * Creates a new Environment with this one as its ancestor.
+	 * @return new Environment
+	 */
 	public Environment beginScope() {
 		return new Environment(this);
 	};
 	
+	/**
+	 * TODO Check what this actually is supposed to do.
+	 * Destroys Environment
+	 * @return ?
+	 */
 	public Environment endScope() {
 		if(ancestor != null)
 			return ancestor;
