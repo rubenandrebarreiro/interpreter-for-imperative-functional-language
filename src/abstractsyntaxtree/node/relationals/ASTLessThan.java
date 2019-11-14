05210773271e223e5129a1158acf0b6ed0c852ca
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-11-14 15:03:13 +0000

    Some minor changes added

--- src/abstractsyntaxtree/node/relationals/ASTLessThan.java
+++ src/abstractsyntaxtree/node/relationals/ASTLessThan.java
@@ -1,5 +1,56 @@
 package abstractsyntaxtree.node.relationals;
 
-public class ASTLessThan {
+import abstractsyntaxtree.exceptions.ASTInvalidIdentifierException;
+import abstractsyntaxtree.node.ASTNode;
+import abstractsyntaxtree.scopes.Environment;
+import abstractsyntaxtree.scopes.compiler.EnvironmentCompiler;
+import abstractsyntaxtree.scopes.compiler.instructions.CodeBlockInstructionsSet;
+import values.atomic.IValue;
+import values.exceptions.TypeErrorException;
+
+public class ASTLessThan implements ASTNode {
+
+	// Global Variables:
+	
+	/**
+	 * The left A.S.T. Node descendant
+	 */
+	private ASTNode leftASTNodeDescendant;
+	
+	/**
+	 * The right A.S.T. Node descendant
+	 */
+	private ASTNode rightASTNodeDescendant;
+	
+	
+	// Constructors:
+	
+	/**
+	 * Constructor #1:
+	 * - The Constructor of a Node of an Abstract Syntax Tree (A.S.T.).
+	 * 
+	 * @param leftASTNodeDescedant the left side Descendant of the A.S.T. Node
+	 * @param rightASTNodeDescedant the left side Descendant of the A.S.T. Node
+	 */
+	public ASTLessThan(ASTNode leftASTNodeDescedant, ASTNode rightASTNodeDescedant) {
+		this.leftASTNodeDescendant = leftASTNodeDescedant;
+		this.rightASTNodeDescendant = rightASTNodeDescedant;
+	}
+	
+	@Override
+	public IValue<?> eval(Environment environment)
+				throws ASTInvalidIdentifierException, TypeErrorException {
+		
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void compile(EnvironmentCompiler environmentCompiler,
+						CodeBlockInstructionsSet codeBlockInstructionsSet)
+										throws ASTInvalidIdentifierException {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
