6158efe0a4bf85a60dc166ae4af7eae7ecc2812e
Eduardo Silva <emf.silva@campus.fct.unl.pt>
2019-11-15 00:22:18 +0000

    Got tests giving results

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -19,64 +19,57 @@ import org.junit.runners.Parameterized.Parameters;
 import main.java.abstractsyntaxtree.exceptions.ASTInvalidIdentifierException;
 import main.java.abstractsyntaxtree.node.ASTNode;
 import main.java.abstractsyntaxtree.scopes.Environment;
+import main.java.compiler.Compiler;
 import main.java.interpreter.parser.*;
 import main.java.values.atomic.IValue;
 import main.java.values.exceptions.TypeErrorException; 
 
 @RunWith(Parameterized.class)
 public class Tester {
-	
+
 	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 	private final PrintStream originalOut = System.out;
 	private final PrintStream originalErr = System.err;
-	
+
 	private String expected;
 	private ByteArrayInputStream input;
 	private InterpreterParser parser;
-	
-    @Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][] {     
-                 { "2+2", "4" },
-                 { "4-2", "2" },
-                 { "-2+12", "10" },
-                 { "5*5", "25" }, 
-                 { "-5*2", "-10" },
-                 { "3*2/4", "0" },
-                 { "(3*2)/4", "1" },
-                 { "Let x=2 in x+2 end", "4"}
-           });
-    }
-	
-	@Before
-	public void setUpStreams() {
-	    System.setOut(new PrintStream(outContent));
-	    System.setErr(new PrintStream(errContent));
+
+	@Parameters
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] {     
+			{ "2+2", "4" },
+			{ "4-2", "2" },
+			{ "-2+12", "10" },
+			{ "5*5", "25" }, 
+			{ "-5*2", "-10" },
+			{ "3*2/4", "0" },
+			{ "(3*2)/4", "1" },
+			{ "Let x=2 in x+2 end", "4"}
+		});
 	}
 
+	@Before
+	public void setUpStreams() {
+		System.setOut(new PrintStream(outContent));
+		System.setErr(new PrintStream(errContent));
+	}
+	
 	@After
 	public void restoreStreams() {
-	    System.setOut(originalOut);
-	    System.setErr(originalErr);
+		System.setOut(originalOut);
+		System.setErr(originalErr);
 	}
-	  
+
 	public Tester(String input, String expected) {
 		this.input = new ByteArrayInputStream((input+"\n").getBytes());
 		this.expected = expected;
-//		parser = new InterpreterParser(this.input);
 	}
-	
+
 	@Test
 	public void test1() throws ASTInvalidIdentifierException, TypeErrorException, ParseException {
-		System.setIn(input);
-		if(parser == null) {
-			parser = new InterpreterParser(input);
-		} else parser.ReInit(input);
-
-		Environment env = new Environment();
-        ASTNode exp = parser.Start();
-		exp.eval(env).show();
+		InterpreterParser.test(input);
 		setUpStreams();
 		assertEquals(expected, outContent.toString().trim());
 		restoreStreams();
