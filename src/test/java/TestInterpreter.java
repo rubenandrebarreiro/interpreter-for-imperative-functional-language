553215329474069ff1edf557fba2b7ec223825a8
Eduardo Silva <emf.silva@campus.fct.unl.pt>
2019-11-15 02:42:34 +0000

    Fix various tests problems

--- src/test/java/TestInterpreter.java
+++ src/test/java/TestInterpreter.java
@@ -20,7 +20,7 @@ import main.java.interpreter.parser.*;
 import main.java.values.exceptions.TypeErrorException; 
 
 @RunWith(Parameterized.class)
-public class InterpreterTester {
+public class TestInterpreter {
 
 	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
@@ -31,8 +31,9 @@ public class InterpreterTester {
 	private ByteArrayInputStream input;
 
 	@Parameters
-	public static Collection<Object[]> arithmetic() {
+	public static Collection<Object[]> data() {
 		return Arrays.asList(new Object[][] {     
+			//Basic Arithmetic
 			{ "2+2", "4" },
 			{ "4-2", "2" },
 			{ "-2+12", "10" },
@@ -40,9 +41,18 @@ public class InterpreterTester {
 			{ "6/2", "3" },
 			{ "-5*2", "-10" },
 			{ "3*2/4", "0" },
-			{ "(3*2)/4", "1" },
-			{ "(20+5000)*0+3-1+3*(-5*(-1))", "17"},
-			{ "(20+5000)*0+((3-1+3*(-5*(-1))))", "17"}
+			{ "(3*2)/4", "1" }, //Nº8
+			//Let operations
+			{ "(20+5000)*0+3-1+3*(-5*(-1))", "17"}, //Nº9
+			{ "(20+5000)*0+((3-1+3*(-5*(-1))))", "17"},
+			{ "Let x=2 in x end", "2"},
+			{ "Let x=2 in 5 end", "5"},
+			{ "Let x=2 y=5 in x*y end", "10"},
+			{ "Let x=10 y=5 z=x+y in z*((-2)+60) end", "870"},
+			{ "Let x=5 in Let y=5 in x*y end end", "25" },
+			{ "Let x=5 in Let y=5 in Let x=2 in y-x end end end", "3" },
+			{ "Let x=2 y=3 in Let z=x+y in Let q=10 x=10 e=q+x in x+e-(z*2) end end end", "20"}, //10+20-(5*2)
+			{ "Let x=2 y=3 in Let x=20 in x+y end +x end", "25"} //Nº18
 		});
 	}
 
@@ -58,16 +68,14 @@ public class InterpreterTester {
 		System.setErr(originalErr);
 	}
 
-	public InterpreterTester(String input, String expected) {
+	public TestInterpreter(String input, String expected) {
 		this.input = new ByteArrayInputStream((input+"\n").getBytes());
 		this.expected = expected;
 	}
 
 	@Test
-	public void test1() throws ASTInvalidIdentifierException, TypeErrorException, ParseException {
-		InterpreterParser.test(input);
-		setUpStreams();
+	public void interpreter() throws ASTInvalidIdentifierException, TypeErrorException, ParseException {
+		InterpreterParser.testInterpreter(input);
 		assertEquals(expected, outContent.toString().trim());
-		restoreStreams();
 	}
 }
