e50e882915eae0b7f6a25fa25cab29cb12777186
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-11-18 14:11:02 +0000

    Some minor changes added

--- src/test/java/TestInterpreter.java
+++ src/test/java/TestInterpreter.java
@@ -59,7 +59,6 @@ public class TestInterpreter {
 			{ "Let x=2 y=3 in Let x=20 in x+y end +x end", "25" }, // #17
 			
 			// Relational operations
-			/*
 			{ "3>4", "false"}, // #18
 			{ "10>8", "true"}, // #19
 			{ "10>10", "false"}, // #20
@@ -67,8 +66,6 @@ public class TestInterpreter {
 			{ "10<10", "false"}, // #22
 			{ "10<=10", "true"}, // #23
 			{ "4>3", "true"} // #24
-			*/
-			
 		});
 	}
 
