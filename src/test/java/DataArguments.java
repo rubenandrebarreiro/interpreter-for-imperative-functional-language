d185e0d26ebfa83d3db7f5c3962cfe28b80727e4
Eduardo Silva <emf.silva@campus.fct.unl.pt>
2019-11-26 12:48:14 +0000

    Adds new test to fix

--- src/test/java/DataArguments.java
+++ src/test/java/DataArguments.java
@@ -51,6 +51,7 @@ public class DataArguments {
 			{ "0==0", "true"}, // #33
 			{ "1==1", "true"},  // #34
 			{ "3==1", "false"}, // #35
+			{ "false > true", "0"},
 			
 			// Logical operations
 			{ "true && true", "true" }, // #36
