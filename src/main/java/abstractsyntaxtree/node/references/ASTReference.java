ddb354f9dcb22da723d2339d710a1967bf914ea6
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-11-28 15:23:10 +0000

    ASTNode for Reference added

--- src/main/java/abstractsyntaxtree/node/references/ASTReference.java
+++ src/main/java/abstractsyntaxtree/node/references/ASTReference.java
@@ -6,21 +6,33 @@ import main.java.abstractsyntaxtree.scopes.Environment;
 import main.java.abstractsyntaxtree.scopes.compiler.EnvironmentCompiler;
 import main.java.abstractsyntaxtree.scopes.compiler.instructions.CodeBlockInstructionsSet;
 import main.java.values.atomic.IValue;
+import main.java.values.exceptions.NumberArgumentsErrorException;
 import main.java.values.exceptions.TypeErrorException;
 
 public class ASTReference implements ASTNode {
 
+	private ASTNode referenceValue;
+	
+	
+	public ASTReference(ASTNode referenceValue) {
+		this.referenceValue = referenceValue;
+	}
+	
+	
 	@Override
-	public IValue<?> eval(Environment<?> environment) throws ASTInvalidIdentifierException, TypeErrorException {
-		// TODO Auto-generated method stub
-		return null;
+	public IValue<?> eval(Environment<?> environment)
+		   throws ASTInvalidIdentifierException, TypeErrorException,
+		          NumberArgumentsErrorException {
+		
+		return this.referenceValue.eval(environment);
+	
 	}
 
 	@Override
 	public void compile(EnvironmentCompiler environmentCompiler, CodeBlockInstructionsSet codeBlockInstructionsSet)
 			throws ASTInvalidIdentifierException {
-		// TODO Auto-generated method stub
+		
+		
 		
 	}
-
 }
