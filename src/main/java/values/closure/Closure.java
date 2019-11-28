6ef0974d84ff390607c62102073d7ecfe4cec257
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-11-28 12:23:13 +0000

    Closure IValue added

---
+++ src/main/java/values/closure/Closure.java
@@ -0,0 +1,33 @@
+package main.java.values.closure;
+
+import java.util.List;
+
+import main.java.abstractsyntaxtree.node.ASTNode;
+import main.java.abstractsyntaxtree.scopes.Environment;
+import main.java.values.atomic.IValue;
+
+public class Closure implements IValue {
+
+	// TODO generic type of Env
+	private Environment env;
+	
+	private List<String> arguments;
+	
+	private ASTNode body;
+	
+	
+	public Closure(Environment<?> env, List<String> arguments, ASTNode body) {
+	
+		this.env = env;
+		this.arguments = arguments;
+		this.body = body;
+	
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
