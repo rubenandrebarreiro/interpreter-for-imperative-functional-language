9da4f07c6ad1e641687ed8a164f52f1e1891ca70
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-12-12 12:10:47 +0000

    AST Assignment added

---
+++ src/main/java/abstractsyntaxtree/node/operators/unary/assignment/ASTAssignment.java
@@ -0,0 +1,62 @@
+package main.java.abstractsyntaxtree.node.operators.unary.assignment;
+
+import main.java.abstractsyntaxtree.exceptions.ASTDuplicatedIdentifierException;
+import main.java.abstractsyntaxtree.exceptions.ASTInvalidIdentifierException;
+import main.java.abstractsyntaxtree.node.ASTNode;
+import main.java.scopes.Environment;
+import main.java.scopes.compiler.EnvironmentCompiler;
+import main.java.scopes.compiler.instructions.CodeBlockInstructionsSet;
+import main.java.types.IType;
+import main.java.values.atomics.IValue;
+import main.java.values.utils.exceptions.NumberArgumentsErrorException;
+import main.java.values.utils.exceptions.TypeErrorException;
+
+public class ASTAssignment implements ASTNode {
+
+	private static final String TYPE_ERROR_MESSAGE = 
+			"Illegal arguments to Assignment operator:\n"
+		  + "- The value used in the respectively Reference it's not of Reference Type!!!";
+	
+	private ASTNode assignmentID;
+	
+	private ASTNode assignmentValue;
+	
+	
+	public ASTAssignment(ASTNode assignmentID, ASTNode assignmentValue) {
+		
+		this.assignmentID = assignmentID;
+		this.assignmentValue = assignmentValue;
+		
+	}
+	
+	@Override
+	public IValue eval(Environment<IValue> environment)
+			throws ASTInvalidIdentifierException, TypeErrorException, NumberArgumentsErrorException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void compile(EnvironmentCompiler environmentCompiler, CodeBlockInstructionsSet codeBlockInstructionsSet)
+		   throws ASTInvalidIdentifierException {
+		
+		this.assignmentID.compile(environmentCompiler, codeBlockInstructionsSet);
+		
+		codeBlockInstructionsSet.addCodeInstruction("checkcast ref_int"); //TODO
+		
+		this.assignmentValue.compile(environmentCompiler, codeBlockInstructionsSet);
+		
+		codeBlockInstructionsSet.addCodeInstruction("putfield ref_int/v I"); //TODO
+		
+	}
+
+	@Override
+	public IType typecheck(Environment<IType> environment) throws TypeErrorException, ASTInvalidIdentifierException,
+		   NumberArgumentsErrorException, ASTDuplicatedIdentifierException {
+			return null;
+
+		// TODO
+		
+	}
+
+}
