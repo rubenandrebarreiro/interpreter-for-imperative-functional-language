634b090425edb2db04997300c3fea17e657be120
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-12-20 17:54:11 +0000

    Some improvements in Unary Mathematics Operators added

--- src/main/java/abstractsyntaxtree/node/operators/unary/mathematics/ASTPostFixIncrement.java
+++ src/main/java/abstractsyntaxtree/node/operators/unary/mathematics/ASTPostFixIncrement.java
@@ -1,5 +1,20 @@
 package main.java.abstractsyntaxtree.node.operators.unary.mathematics;
 
+/**
+ * Interpreter for Imperative/Functional Language
+ * 
+ * Interpretation and Compilation of Programming Languages
+ * 
+ * Faculty of Science and Technology of New University of Lisbon
+ * (FCT NOVA | FCT/UNL)
+ * 
+ * Integrated Master of Computer Science and Engineering
+ * (BSc. + MSc. Bologna Degree)
+ * 
+ * Academic Year 2019/2020
+ * 
+ */
+
 import main.java.abstractsyntaxtree.exceptions.ASTDuplicatedIdentifierException;
 import main.java.abstractsyntaxtree.exceptions.ASTInvalidIdentifierException;
 import main.java.abstractsyntaxtree.node.ASTNode;
@@ -13,6 +28,15 @@ import main.java.values.mathematics.VInt;
 import main.java.values.utils.exceptions.NumberArgumentsErrorException;
 import main.java.values.utils.exceptions.TypeErrorException;
 
+/**
+ * Class for the Node of an Abstract Syntax Tree, representing a Post Fix Increment.
+ * 
+ * @supervisor Prof. Luis Manuel Caires - lcaires@fct.unl.pt
+ * 
+ * @author Eduardo Bras Silva (no. 41798) - emf.silva@campus.fct.unl.pt
+ * @author Ruben Andre Barreiro (no. 42648) - r.barreiro@campus.fct.unl.pt
+ *
+ */
 public class ASTPostFixIncrement implements ASTNode {
 
 	// Global Instance Variables:
@@ -45,16 +69,33 @@ public class ASTPostFixIncrement implements ASTNode {
 	}
 	
 	
-	// Methods:
+	// Methods/Functions:
 
 	/**
-	 * Evaluates the current Node of an Abstract Syntax Tree, returning its Atomic Number.
+	 * Evaluates the Expression of the current Node of an Abstract Syntax Tree (A.S.T.),
+	 * given the Environment (Scope), where the current A.S.T. Node it's inside.
 	 * 
-	 * @param environment the Environment of the Scope/Frame, containing this A.S.T. Node (Num)
+	 * @param environment the Environment (Scope), where the current A.S.T. Node it's inside
+	 * 
+	 * @return the evaluation of the Expression of the current Node of an Abstract Syntax Tree (A.S.T.),
+	 *  	   given the Environment (Scope), where the current A.S.T. Node it's inside        
+	 *
+	 * @throws ASTInvalidIdentifierException an Invalid Identifier Exception thrown,
+	 * 		   in the case of an Identifier it's completely unknown in the
+	 * 		   Environment's ancestor on the Stack of Environments (Scopes/Frames)
+	 * 
+	 * @throws TypeErrorException a Type Error Exception thrown,
+	 * 		   in the case of the Type of a Value it's completely unknown to
+	 * 		   the recognised and acceptable Types for Values
+	 * 
+	 * @throws NumberArgumentsErrorException a raised Number of Arguments Error Exception,
+	 * 		   in the case of the Number of Arguments used in the Evaluation,
+	 *         wrong in the current Environment of Values (Scope/Frame) being evaluated
+	 *         
 	 */
 	@Override
 	public IValue eval(Environment<IValue> environment)
-			throws ASTInvalidIdentifierException, TypeErrorException, NumberArgumentsErrorException {
+		   throws ASTInvalidIdentifierException, TypeErrorException, NumberArgumentsErrorException {
 
 		// Returns A.S.T. Node, representing an Atomic Number PostFix Incremented
 		VInt numASTNodeValueToBeIncremented = this.numASTNodeValueToBeIncremented;
@@ -67,17 +108,61 @@ public class ASTPostFixIncrement implements ASTNode {
 		
 	}
 
+	/**
+	 * Compiles the List of Code Instructions of the current Node of an Abstract Syntax Tree (A.S.T.),
+	 * given the Environment (Scope/Frame), where the current A.S.T. Node it's inside and
+	 * the List of the Code Instructions of the current Node of an
+	 * Abstract Syntax Tree (A.S.T.) will be kept, writing J.V.M. instructions.
+	 * 
+	 * @param environment the Environment (Scope/Frame), where the current Code Instructions of
+	 *        the current Node of an Abstract Syntax Tree (A.S.T.) will be kept
+	 * 
+	 * @param codeInstructions the List of the Code Instructions to be compiled
+	 * 
+	 * @throws ASTInvalidIdentifierException an Invalid Identifier Exception thrown,
+	 * 		   in the case of an Identifier it's completely unknown in the
+	 * 		   Environment's ancestor on the Stack of Environments (Scopes/Frames) 
+	 * 
+	 */
 	@Override
 	public void compile(EnvironmentCompiler environmentCompiler, CodeBlockInstructionsSet codeBlockInstructionsSet)
 			throws ASTInvalidIdentifierException {
 		
 		codeBlockInstructionsSet.addCodeInstruction
-		("sipush " + String.valueOf(numASTNodeValueToBeIncremented.getValue()));
+								("sipush " + String.valueOf(numASTNodeValueToBeIncremented.getValue()));
 		
 		codeBlockInstructionsSet.addCodeInstruction("iinc");
 		
 	}
 
+	/**
+	 * Performs the Typechecking for the Type associated to this A.S.T. Node Identifier,
+	 * performing the Typecheking on it and in its descendants A.S.T. Nodes,
+	 * verifying the Type of the Values of all the A.S.T. Nodes.
+	 * 
+	 * @param environment the Environment (Scope/Frame), where the types of
+	 *        the current Node of an Abstract Syntax Tree (A.S.T.) will be evaluated,
+	 *        in a Static Typechecking, before runtime of the program
+	 * 
+	 * @throws TypeErrorException a Type Error Exception thrown,
+	 * 		   in the case of a Type used for in Typechecking of an A.S.T. Node it's
+	 * 		   wrong in the current Environment of Types (Scope/Frame) being evaluated
+	 *
+	 * @throws ASTInvalidIdentifierException an Invalid Identifier Exception thrown,
+	 * 		   in the case of an Identifier it's completely unknown in the
+	 * 		   Environment's ancestor on the Stack of Environments (Scopes/Frames) 
+	 * 
+	 * @throws NumberArgumentsErrorException a Number of Arguments Error Exception thrown,
+	 *         in the case of the Number of Arguments used in the Typechecking,
+	 *         wrong in the current Environment of Types (Scope/Frame) being evaluated
+	 *         
+	 * @throws ASTDuplicatedIdentifierException a Duplicated Identifier Exception thrown,
+	 * 		   in the case of more than one certain Identifier it's found,
+	 *         in the current Environment of Types (Scope/Frame) being evaluated
+	 *
+	 * @return the Type for the A.S.T. Node, after the Typechecking be performed
+	 * 
+	 */
 	@Override
 	public IType typecheck(Environment<IType> environment) throws TypeErrorException, ASTInvalidIdentifierException,
 			NumberArgumentsErrorException, ASTDuplicatedIdentifierException {
