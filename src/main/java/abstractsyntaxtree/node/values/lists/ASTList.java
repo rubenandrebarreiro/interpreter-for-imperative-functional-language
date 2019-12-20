cd48efdea23fc18ee22ebf7a00d8c19e3fa03551
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-12-20 20:01:04 +0000

    Some minor changes/improvements in List Values added

--- src/main/java/abstractsyntaxtree/node/values/lists/ASTList.java
+++ src/main/java/abstractsyntaxtree/node/values/lists/ASTList.java
@@ -1,5 +1,29 @@
 package main.java.abstractsyntaxtree.node.values.lists;
 
+/**
+ * Interpreter for Imperative/Functional Language
+ * 
+ * Interpretation and Compilation of Programming Languages
+ * 
+ * Faculty of Science and Technology of New University of Lisbon
+ * (FCT NOVA | FCT/UNL)
+ * 
+ * Integrated Master of Computer Science and Engineering
+ * (BSc. + MSc. Bologna Degree)
+ * 
+ * Academic Year 2019/2020
+ * 
+ */
+
+/**
+ * Class for the Node of an Abstract Syntax Tree, representing a List.
+ * 
+ * @supervisor Prof. Luis Manuel Caires - lcaires@fct.unl.pt
+ * 
+ * @author Eduardo Bras Silva (no. 41798) - emf.silva@campus.fct.unl.pt
+ * @author Ruben Andre Barreiro (no. 42648) - r.barreiro@campus.fct.unl.pt
+ *
+ */
 public class ASTList {
 
 }
