f6dd8a058862a88df6e1db1bda01c69b1ba7d0aa
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-12-20 20:03:00 +0000

    Some minor changes/improvements in Environment Compiler added

--- src/main/java/scopes/compiler/EnvironmentCompiler.java
+++ src/main/java/scopes/compiler/EnvironmentCompiler.java
@@ -15,42 +15,81 @@ package main.java.scopes.compiler;
  * 
  */
 
+/**
+ * Class for the Environment Compiler.
+ * 
+ * @supervisor Prof. Luis Manuel Caires - lcaires@fct.unl.pt
+ * 
+ * @author Eduardo Bras Silva (no. 41798) - emf.silva@campus.fct.unl.pt
+ * @author Ruben Andre Barreiro (no. 42648) - r.barreiro@campus.fct.unl.pt
+ *
+ */
 public class EnvironmentCompiler {
 	
 	// Global Instance Variables:
 
 	/**
-	 * The Static Link pointer to its Ancestor
+	 * The Static Link pointer to its Environment Compiler (Scope/Frame) Ancestor
 	 */
 	private EnvironmentCompiler staticLinkAncestorHeapStackFrame;
 
 	/**
-	 * The Current number used for labels Lx
+	 * The current number used for labels "Lx"
 	 */
 	private int currentLabelNumber;
 	
+	
 	// Constructors:
+	
 	/**
-	 * 
+	 * Constructor #1:
+	 * - The constructor of an Environment Compiler,
+	 *   for a Scope/Frame without any ancestor;
 	 */
 	public EnvironmentCompiler() {		
 		this.staticLinkAncestorHeapStackFrame = null;
 		this.currentLabelNumber = 0;
 	}
 
+	/**
+	 * Constructor #2:
+	 * - The constructor of an Environment Compiler,
+	 *   for a Scope/Frame with, at least, one ancestor;
+	 *   
+	 * @param ancestorEnvironment the Environment Compiler of the ancestor
+	 */
 	public EnvironmentCompiler(EnvironmentCompiler ancestorEnvironment) {		
 		this.staticLinkAncestorHeapStackFrame = ancestorEnvironment;
 		this.currentLabelNumber = 0;
 	}
 	
+	/**
+	 * Returns this Environment Compiler, starting a new Scope/Frame.
+	 * 
+	 * @return this Environment Compiler, starting a new Scope/Frame
+	 */
 	public EnvironmentCompiler beginScope() {
 		return new EnvironmentCompiler(this);
 	}
 
+	/**
+	 * Returns true, if this Environment Compiler (Scope/Frame)
+	 * it's the root and don't have any Environment Compiler (Scope/Frame) ancestors
+	 * and false, otherwise.
+	 * 
+	 * @return true, if this Environment Compiler (Scope/Frame)
+	 * 		   it's the root and don't have any Environment Compiler (Scope/Frame) ancestors
+	 * 		   and false, otherwise
+	 */
 	public boolean isEnvironmentRoot() {
 		return this.staticLinkAncestorHeapStackFrame == null ? true : false;
 	}
 	
+	/**
+	 * Returns the current number used for labels "Lx".
+	 * 
+	 * @return the current number used for labels "Lx"
+	 */
 	public int getCurrentLabelNumber() {
 		return currentLabelNumber++;
 	}
