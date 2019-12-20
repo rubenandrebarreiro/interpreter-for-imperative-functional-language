22044d4cb30707b990f66eee65a9ac0628c4060e
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-12-20 18:20:55 +0000

    Some minor fixes added

--- src/main/java/abstractsyntaxtree/node/operators/unary/print/ASTPrint.java
+++ src/main/java/abstractsyntaxtree/node/operators/unary/print/ASTPrint.java
@@ -1,5 +1,20 @@
 package main.java.abstractsyntaxtree.node.operators.unary.print;
 
+/**
+ * Interpreter for Imperative/Functional Language
+ * 
+ * Interpretation and Compilation of Programming Languages
+ * 
+ * Faculty of Science and Technology of New University of Lisbon
+ * (FCT NOVA | FCT/UNL)
+ * 
+ * Integrated Master of Computer Science and Engineering
+ * (BSc. + MSc. Bologna Degree)
+ * 
+ * Academic Year 2019/2020
+ * 
+ */
+
 import main.java.abstractsyntaxtree.exceptions.ASTDuplicatedIdentifierException;
 import main.java.abstractsyntaxtree.exceptions.ASTInvalidIdentifierException;
 import main.java.abstractsyntaxtree.node.ASTNode;
@@ -11,8 +26,22 @@ import main.java.values.atomics.IValue;
 import main.java.values.utils.exceptions.NumberArgumentsErrorException;
 import main.java.values.utils.exceptions.TypeErrorException;
 
+/**
+ * Class for the Node of an Abstract Syntax Tree, representing a Print operation.
+ * 
+ * @supervisor Prof. Luis Manuel Caires - lcaires@fct.unl.pt
+ * 
+ * @author Eduardo Bras Silva (no. 41798) - emf.silva@campus.fct.unl.pt
+ * @author Ruben Andre Barreiro (no. 42648) - r.barreiro@campus.fct.unl.pt
+ *
+ */
 public class ASTPrint implements ASTNode {
-
+	
+	// Global Instance Variables:
+	
+	/**
+	 * The A
+	 */
 	private ASTNode stringToPrint;
 	
 	
@@ -20,6 +49,31 @@ public class ASTPrint implements ASTNode {
 		this.stringToPrint = stringToPrint;
 	}
 	
+	
+	// Methods/Functions:
+	
+	/**
+	 * Evaluates the Expression of the current Node of an Abstract Syntax Tree (A.S.T.),
+	 * given the Environment (Scope), where the current A.S.T. Node it's inside.
+	 * 
+	 * @param environment the Environment (Scope), where the current A.S.T. Node it's inside
+	 * 
+	 * @return the evaluation of the Expression of the current Node of an Abstract Syntax Tree (A.S.T.),
+	 *  	   given the Environment (Scope), where the current A.S.T. Node it's inside        
+	 *
+	 * @throws ASTInvalidIdentifierException an Invalid Identifier Exception thrown,
+	 * 		   in the case of an Identifier it's completely unknown in the
+	 * 		   Environment's ancestor on the Stack of Environments (Scopes/Frames)
+	 * 
+	 * @throws TypeErrorException a Type Error Exception thrown,
+	 * 		   in the case of the Type of a Value it's completely unknown to
+	 * 		   the recognised and acceptable Types for Values
+	 * 
+	 * @throws NumberArgumentsErrorException a raised Number of Arguments Error Exception,
+	 * 		   in the case of the Number of Arguments used in the Evaluation,
+	 *         wrong in the current Environment of Values (Scope/Frame) being evaluated
+	 *         
+	 */
 	@Override
 	public IValue eval(Environment<IValue> environment)
 			throws ASTInvalidIdentifierException, TypeErrorException, NumberArgumentsErrorException {
@@ -29,18 +83,64 @@ public class ASTPrint implements ASTNode {
 		return null;
 	}
 
+	/**
+	 * Compiles the List of Code Instructions of the current Node of an Abstract Syntax Tree (A.S.T.),
+	 * given the Environment (Scope/Frame), where the current A.S.T. Node it's inside and
+	 * the List of the Code Instructions of the current Node of an
+	 * Abstract Syntax Tree (A.S.T.) will be kept, writing J.V.M. instructions.
+	 * 
+	 * @param environment the Environment (Scope/Frame), where the current Code Instructions of
+	 *        the current Node of an Abstract Syntax Tree (A.S.T.) will be kept
+	 * 
+	 * @param codeInstructions the List of the Code Instructions to be compiled
+	 * 
+	 * @throws ASTInvalidIdentifierException an Invalid Identifier Exception thrown,
+	 * 		   in the case of an Identifier it's completely unknown in the
+	 * 		   Environment's ancestor on the Stack of Environments (Scopes/Frames) 
+	 * 
+	 */
 	@Override
 	public void compile(EnvironmentCompiler environmentCompiler, CodeBlockInstructionsSet codeBlockInstructionsSet)
-			throws ASTInvalidIdentifierException {
+		   throws ASTInvalidIdentifierException {
 
-		codeBlockInstructionsSet.addCodeInstruction("getstatic java/lang/System/out Ljava/io/PrintStream;");
+		codeBlockInstructionsSet.addCodeInstruction
+								(String.format("getstatic java/lang/System/out Ljava/io/PrintStream;"));
 		
 		this.stringToPrint.compile(environmentCompiler, codeBlockInstructionsSet);
 		
-		codeBlockInstructionsSet.addCodeInstruction("invokevirtual java/io/PrintStream/println(Ljava/lang/String)V");
+		codeBlockInstructionsSet.addCodeInstruction
+								(String.format("invokevirtual java/io/PrintStream/println(Ljava/lang/String)V"));
 		
 	}
 
+	/**
+	 * Performs the Typechecking for the Type associated to this A.S.T. Node Identifier,
+	 * performing the Typecheking on it and in its descendants A.S.T. Nodes,
+	 * verifying the Type of the Values of all the A.S.T. Nodes.
+	 * 
+	 * @param environment the Environment (Scope/Frame), where the types of
+	 *        the current Node of an Abstract Syntax Tree (A.S.T.) will be evaluated,
+	 *        in a Static Typechecking, before runtime of the program
+	 * 
+	 * @throws TypeErrorException a Type Error Exception thrown,
+	 * 		   in the case of a Type used for in Typechecking of an A.S.T. Node it's
+	 * 		   wrong in the current Environment of Types (Scope/Frame) being evaluated
+	 *
+	 * @throws ASTInvalidIdentifierException an Invalid Identifier Exception thrown,
+	 * 		   in the case of an Identifier it's completely unknown in the
+	 * 		   Environment's ancestor on the Stack of Environments (Scopes/Frames) 
+	 * 
+	 * @throws NumberArgumentsErrorException a Number of Arguments Error Exception thrown,
+	 *         in the case of the Number of Arguments used in the Typechecking,
+	 *         wrong in the current Environment of Types (Scope/Frame) being evaluated
+	 *         
+	 * @throws ASTDuplicatedIdentifierException a Duplicated Identifier Exception thrown,
+	 * 		   in the case of more than one certain Identifier it's found,
+	 *         in the current Environment of Types (Scope/Frame) being evaluated
+	 *
+	 * @return the Type for the A.S.T. Node, after the Typechecking be performed
+	 * 
+	 */
 	@Override
 	public IType typecheck(Environment<IType> environment)
 	       throws TypeErrorException,
@@ -52,5 +152,5 @@ public class ASTPrint implements ASTNode {
 		return this.stringToPrint.typecheck(environment);
 		
 	}
-
+	
 }
