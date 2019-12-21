028aa67379e93985352fb264eaa877bc6ef79d6f
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-12-21 00:26:26 +0000

    Tuples and Triples Nodes added

---
+++ src/main/java/abstractsyntaxtree/node/primitives/binary/ATSTTuple.java
@@ -0,0 +1,66 @@
+package main.java.abstractsyntaxtree.node.primitives.binary;
+
+/**
+ * Interpreter for Imperative/Functional Language
+ * 
+ * Interpretation and Compilation of Programming Languages
+ * 
+ * Faculty of Science and Technology of New University of Lisbon
+ * (FCT NOVA | FCT/UNL)
+ * 
+ * Integrated Master of Computer Science and Engineering
+ * (BSc. + MSc. Bologna Degree)
+ * 
+ * Academic Year 2019/2020
+ * 
+ */
+
+import main.java.abstractsyntaxtree.exceptions.ASTDuplicatedIdentifierException;
+import main.java.abstractsyntaxtree.exceptions.ASTInvalidIdentifierException;
+import main.java.abstractsyntaxtree.node.ASTNode;
+import main.java.scopes.Environment;
+import main.java.scopes.compiler.EnvironmentCompiler;
+import main.java.scopes.compiler.instructions.CodeBlockInstructionsSet;
+import main.java.types.IType;
+import main.java.values.atomics.IValue;
+import main.java.values.utils.exceptions.NumberArgumentsErrorException;
+import main.java.values.utils.exceptions.TypeErrorException;
+
+/**
+ * Class for the Node of an Abstract Syntax Tree, representing a Tuple primitive.
+ * 
+ * @supervisor Prof. Luis Manuel Caires - lcaires@fct.unl.pt
+ * 
+ * @author Eduardo Bras Silva (no. 41798) - emf.silva@campus.fct.unl.pt
+ * @author Ruben Andre Barreiro (no. 42648) - r.barreiro@campus.fct.unl.pt
+ *
+ */
+public class ATSTTuple implements ASTNode {
+
+	
+	@Override
+	public IValue eval(Environment<IValue> environment)
+		   throws ASTInvalidIdentifierException, TypeErrorException,
+				  NumberArgumentsErrorException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void compile(EnvironmentCompiler environmentCompiler,
+						CodeBlockInstructionsSet codeBlockInstructionsSet)
+								throws ASTInvalidIdentifierException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public IType typecheck(Environment<IType> environment)
+		   throws TypeErrorException, ASTInvalidIdentifierException,
+		   		  NumberArgumentsErrorException, ASTDuplicatedIdentifierException {
+		
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
